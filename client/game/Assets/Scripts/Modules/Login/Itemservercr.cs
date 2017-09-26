4a30c27134309fa711fa30cbdf3a0732a3b8ee2e
feisha10 <feisha10@qq.com>
2017-09-26 11:08:15 +0800

    ugui editor

---
+++ client/game/Assets/Scripts/Modules/Login/Itemservercr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Itemservercr : MonoBehaviour
+{
+
+	private Transform cachedTransform ;
+	//开始UI申明;
+	private Image image_servercr;
+	private Text text_serNamecr;
+	//结束UI申明;
+	private bool isUIinit = false;
+
+	void Awake ()
+	{
+		cachedTransform=transform;
+		//开始UI获取;
+		image_servercr = cachedTransform.Find("image_servercr").GetComponent<Image>();
+		text_serNamecr = cachedTransform.Find("image_servercr/text_serNamecr").GetComponent<Text>();
+		//结束UI获取;
+		isUIinit = true;
+	}
+}
+
